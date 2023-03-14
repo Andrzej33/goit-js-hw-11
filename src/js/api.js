884f8a728345d0d9f6fee549d6d4d3989954e6dd
Andrij Vasylkiv <andrijvasilkiv7@gmail.com>
2023-03-14 14:40:57 +0200

    simpleLightbox add

--- src/js/api.js
+++ src/js/api.js
@@ -1,6 +1,6 @@
 const API_KEY = "34316934-23a1792d471904186ea8894b3";
 const URL = "https://pixabay.com/api/";
-const PER_PAGE = 4;
+const PER_PAGE = 40;
 
 export const getData = async (request,page) => {
     const response = await fetch(`${URL}?key=${API_KEY}&q=${request}&page=${page}&per_page=${PER_PAGE}&image_type="photo"&orientation="horizontal"&safesearch="true"`);
--- src/js/search-images.js
+++ src/js/search-images.js
@@ -1,5 +1,7 @@
 import { getData } from "./api";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 
 let page = 1;
@@ -8,43 +10,52 @@ let query = '';
 let total = 0;
 
 
+var lightbox = new SimpleLightbox('.gallery a', { /* options */ });
 
 
 
 
-
-const form = document.querySelector('.search-form')
-const galery = document.querySelector('.gallery')
-const loadBtn = document.querySelector('.load-more')
-
+const form = document.querySelector('.search-form');
+const galery = document.querySelector('.gallery');
+const loadBtn = document.querySelector('.load-more');
 
 
-form.addEventListener('submit', onSearchClick);
+
+const simpleGallery = new SimpleLightbox('.gallery a',{});
+
+
+hideBtn()
+
+
+
+form.addEventListener('submit', onSearchSubmit);
 loadBtn.addEventListener('click',onBtnClick)
 
 const render = () => {
   
-  const markup = hits.map((hit) => `<div class="photo-card">
-  <img src="${hit.webformatURL}" alt="${hit.tags}" width="120" height="150" loading="lazy" />
-  <div class="info">
-    <p class="info-item">likes
-      <b>${hit.likes}</b>
-    </p>
-    <p class="info-item">views
-      <b>${hit.views}</b>
-    </p>
-    <p class="info-item">comments
-      <b>${hit.comments}</b>
-    </p>
-    <p class="info-item">downloads
-      <b>${hit.downloads}</b>
-    </p>
-  </div>
-</div>`)
+  const markup = hits.map((hit) => `<a class="image-link" href="${hit.largeImageURL}"><div class="photo-card">
+    <img src="${hit.webformatURL}" alt="${hit.tags}"
+    title="${hit.tags}" width="120" height="100" loading="lazy" />
+    <div class="info">
+      <p class="info-item">likes
+        <b>${hit.likes}</b>
+      </p>
+      <p class="info-item">views
+        <b>${hit.views}</b>
+      </p>
+      <p class="info-item">comments
+        <b>${hit.comments}</b>
+      </p>
+      <p class="info-item">downloads
+        <b>${hit.downloads}</b>
+      </p>
+    </div>
+  </div></a>`)
     .join('');
   
   // galery.innerHTML = markup;
-  galery.insertAdjacentHTML('beforeend',markup)
+  galery.insertAdjacentHTML('beforeend', markup);
+  simpleGallery.refresh()
 }
 
 const fetchPictures = () => {
@@ -53,14 +64,16 @@ getData(query,page)
       // const { hits, totalHits, total } = data;
       hits = data.hits;
       total = data.totalHits;
-      // console.log(hits);
-      // console.log(totalHits);
-      console.log(total);
-      if (!hits.length) {
-        Notify.failure('Sorry, there are no images matching your search query. Please try again.')
-        return;
-      }
-    
+      const length = hits.length;
+      // console.log(hits.length);
+      console.log(length);
+      console.log(hits);
+      // if (!length) {
+      //   hideBtn()
+      //   Notify.failure('Sorry, there are no images matching your search query. Please try again.')
+      //   return;
+      // }
+    checkDataLength(length,total)
       // console.log(data),
       render()
     })
@@ -68,30 +81,65 @@ getData(query,page)
 }
 
 
-function onSearchClick(e) {
-    e.preventDefault()
+function onSearchSubmit(e) {
+  e.preventDefault()
+  hideBtn()
+  
   //  inputValue = form.searchQuery.value
   const inputValue = e.currentTarget.elements.searchQuery.value
     // console.log(form.searchQuery.value);
-  if (!inputValue.trim() || inputValue === query) {
+  if (inputValue.trim() === '') {
+    galery.innerHTML = '';
+  // hideBtn()
+  }  if
+  (!inputValue.trim() || inputValue === query){
     return
   }
-  query = inputValue;
-  page = 1;
-  galery.innerHTML = '';
-  fetchPictures()
-    // fetchUsers().then(users => console.log(users))
+  
+  
+    query = inputValue;
+    page = 1;
+    galery.innerHTML = '';
+    fetchPictures()
+   
+  
 }
 
 
 
-// const fetchUsers = async () => {
-//   const response = await fetch("https://pixabay.com/api/?key=34316934-23a1792d471904186ea8894b3&q=yellow+flowers&image_type=photo");
-//   const users = await response.json();
-//   return users;
-// };
 
-function onBtnClick () {
+
+function onBtnClick() {
+  hideBtn()
   page += 1;
   fetchPictures()
+}
+
+function showBtn() {
+  loadBtn.classList.remove('is-hidden')
+
+}
+
+function hideBtn() {
+  loadBtn.classList.add('is-hidden')
+}
+
+
+function checkDataLength(itemsLength,totalAmount) {
+  if (!itemsLength) {
+  
+    Notify.failure('Sorry, there are no images matching your search query. Please try again.')
+    return;
+  }
+  if (itemsLength >= 40) {
+    showBtn()
+  }
+   if 
+  (itemsLength < 40 && page !== 1){
+     Notify.info("We're sorry, but you've reached the end of search results.")
+    
+  }
+   if (page === 1) {
+    Notify.success(`Hooray! We found ${totalAmount} images.`)
+  }
 }
